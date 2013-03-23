52209a801d40fd4549e3f2d6572de7803aad76bf
jav <nathan@jav-baf.(none)>
2013-03-23 20:29:24 +0200

    Add user microposts

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -10,12 +10,17 @@ describe "Authentication" do
     it { should have_selector('h1',    text: 'Sign in') }
     it { should have_selector('title', text: 'Sign in') }
   end
+
   describe "signin" do
+
     before {visit signin_path}
+
     describe "with invalid information" do
+
       before {click_button "Sign in"}
       it {should have_selector('title', text:'Sign in')}
       it {should have_selector('div.alert.alert-error', text: 'Invalid')}
+
       describe "after visiting another page" do
         before { click_link "Home" }
         it { should_not have_selector('div.alert.alert-error') }
@@ -77,6 +82,20 @@ describe "Authentication" do
           it {should have_selector('title', text: 'Sign in')}
         end
       end
+
+      describe "in the Microposts controller" do
+
+        describe "submitting to the create action" do
+          before{post microposts_path}
+          specify {response.should redirect_to(signin_path)}
+        end
+
+        describe "submitting to the destroy action" do
+          before{delete micropost_path(FactoryGirl.create(:micropost))}
+          specify {response.should redirect_to(signin_path)}
+        end
+      end
+
     end
 
     describe "as wrong user" do
